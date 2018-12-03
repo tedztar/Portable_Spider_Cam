19f8356e394c56d167367f0618d551643affe221
Timothy Stevenson <tedztar@gmail.com>
2018-12-04 12:36:19 +1300

    Worked on the math

---
+++ Main process/Math.py
@@ -0,0 +1,25 @@
+#The math is completed
+
+class platform:
+     def __init__(self):
+        self.x=int()
+        self.y=int()
+        self.x=int()
+        self.maxx=int()
+        self.maxy=int()
+        self.maxz=int()
+
+
+class motor:
+        motor1.equation = [ (x-A), (yy-y-A),(zz-z)]
+        motor2.equation = [ (xx-x-A), (yy-y-A),(zz-z)]
+        motor3.equation = [ (x-A), (y-A),(zz-z)]
+        motor4.equation = [ (xx-x-A), (y-A),(zz-z)]
+    def __init__(self):
+        self.length = int()
+
+    def getlength():
+        length = sqrt(self.equation[0]**2+sels.equation[1]**2+self.equation[2]**2)
+  
+
+

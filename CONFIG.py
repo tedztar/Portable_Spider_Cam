31d62ef1ea098f857d5048a4e436cbb6a140e8d1
SmartyJem <31848174+SmartyJem@users.noreply.github.com>
2018-12-21 15:30:59 +1300

    Update CONFIG.py

--- CONFIG.py
+++ CONFIG.py
@@ -11,65 +11,79 @@ def read_file():
 
       
 
-      # Create a catergory Reference List.
-      info = ["catergory1", "catergory2"]
+      # Create a category Reference List.
+      info = ["category1", "category2"]
 
-      # Set the file catergories to the config functions.
+      # Set the file categories to the config functions.
       config.read("file_name.ini")
 
-      # Split the catergories from the file.
-      for catergory in config.sections():
+      # Split the categories from the file.
+      for category in config.sections():
 
-         # Do all catergories exist in the file. 
-         if catergory in info:
-            info.remove(catergory)
+         # Do all categories exist in the file. 
+         if category in info:
+            info.remove(category)
 
-         # If the catergory doesn't exist, remove it.
+         # If the category doesn't exist, remove it.
          else:
-            config.remove_section(catergory)
+            config.remove_section(category)
 
-      # If there isn't a catergory, create the missing catergories.
-      for catergory in info:
-         config.add_section(catergory)
-
-      write_file()
+      # If there isn't a category, create the missing categories.
+      for category in info:
+         config.add_section(category)
 
     
 
-      # Create a sub_catergory reference list.
+      # Create a sub_category reference list.
       info = [
-         ["sub_catergory1", "sub_catergory2"],
-         ["sub_catergory1"],
+         ["sub_category1", "sub_category2"],
+         ["sub_category1"],
          ]
 
       # With the list inside a list reference, it also needs an index.
       index = int(0)
 
-      # Loop each catergory to check if the sub_catergories exist.
-      for catergory in config.sections():
+      # Loop each category to check if the sub_categories exist.
+      for category in config.sections():
 
-            # This chooses the sub_catergories to check against.
+            # This chooses the sub_categories to check against.
             task = info[index]
 
-            # Loop each sub_catergory to check if the sub_catergories is equal.
-            for sub_catergory in config.options(catergory):
+            # Loop each sub_category to check if the sub_categories is equal.
+            for sub_category in config.options(category):
 
-               # Do all sub_catergories exist in the catergory.
-               if sub_catergory in task:
-                  task.remove(sub_catergory)
+               # Do all sub_categories exist in the category.
+               if sub_category in task:
+                  task.remove(sub_category)
 
-               # If the sub_catergory doesn't exist, remove it.
+               # If the sub_category doesn't exist, remove it.
                else:
-                  config.remove_option(catergory, sub_catergory)
+                  config.remove_option(category, sub_category)
 
-            # If the sub catergories don't exist then create them.
-            for sub_catergory in task:
-               config.set(catergory, sub_catergory, "value")
+            # If the sub categories don't exist then create them.
+            for sub_category in task:
+               config.set(category, sub_category, "")
 
             # Once it is done, add one to the index for the next list.
             index += 1
 
+            
+
+      # Check if the values are intergers.
+      for category in config.sections():
+         for sub_category in config.options(category):
+
+            # If the values are not intergers then it will send an error "ValueError".
+            try:
+               int(config.get(category, sub_category))
+               pass
+
+            # Send feedback if a value needs to be changed.
+            except ValueError:
+                   print("{} in {} needs to be configured.".format(sub_category, category))
+
       write_file()
+         
 
 
 
@@ -82,14 +96,14 @@ def read_file():
 # This file does not exist. Create the factory default file.
 def create_file():
 
-   # Create the catergory and set the sub_catergories.
-   config["catergory1"] = {
-      "sub_catergory1" : "value",
-      "sub_catergory2" : "value",
+   # Create the category and set the sub_catergories.
+   config["category1"] = {
+      "sub_category1" : "0",
+      "sub_category2" : "0",
       }
 
-   config["catergory2"] = {
-      "sub_catergory1" : "value",
+   config["category2"] = {
+      "sub_category1" : "0",
       }
 
    write_file()
@@ -104,7 +118,7 @@ def write_file():
    with open("file_name.ini", "w") as file_name:
       # Write the factory default file.
       config.write(file_name)
-      
+
 
 
 # If this program is the main module. Run this code.
@@ -115,3 +129,4 @@ if __name__ == "__main__":
 
    # 1.Step one, check if the config files exist.
    read_file()
+

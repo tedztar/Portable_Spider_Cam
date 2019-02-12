98fa7653e734d813ae00b4536044e8bf1a8f6166
Timothy Stevenson <tedztar@gmail.com>
2019-02-12 17:58:12 +1300

    got controller buttons working

--- Phantom 3 controler test/Pantom_test.py
+++ Phantom 3 controler test/Pantom_test.py
@@ -1,5 +1,6 @@
 import socket
 import struct
+import time
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("192.168.1.1", 2345))
@@ -13,11 +14,15 @@ def conv(w):
 while True:
     data = bytearray(s.recv(1024))
     if len(data) == 26:
-        rightX = word(data[12], data[11])
-        rightY = word(data[14], data[13]) 
-        leftY = word(data[16], data[15]) 
-        leftX = word(data[18], data[17]) 
-        cam = word(data[20], data[19]) 
+#       rightX = word(data[12], data[11])
+#        rightY = word(data[14], data[13]) 
+#        leftY = word(data[16], data[15]) 
+#        leftX = word(data[18], data[17]) 
+#        cam = word(data[20], data[19]) 
 #        print("leftX: %4i, leftY: %4i, rightX: %4i, rightY: %4i, cam: %4i" % (
 #           leftX, leftY, rightX, rightY, cam))
-        print(data[24])
+        #(data[23]>>3)&31=data[23]
+        #print("{0:b}".format(data[23]))
+        #time.sleep(3)
+        buttons = [bool(int(x)) for x in bin((data[23] >> 3)&31)[2:].zfill(5)]
+        print(buttons)

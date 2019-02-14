192795f632498b9159b50c7386d77b518680a1c5
Timothy Stevenson <tedztar@gmail.com>
2019-02-15 08:55:46 +1300

    Stuff

--- Contoller scripts/Pantom_test.py
+++ Contoller scripts/Pantom_test.py
@@ -11,18 +11,28 @@ def word(b0, b1):
 def conv(w):
     return (w / 8 - 128) & 0xff
 
+def scale(x):
+                return int((x - 364) / 1320 * 32768)
+
 while True:
     data = bytearray(s.recv(1024))
     if len(data) == 26:
-#       rightX = word(data[12], data[11])
-#        rightY = word(data[14], data[13]) 
-#        leftY = word(data[16], data[15]) 
-#        leftX = word(data[18], data[17]) 
-#        cam = word(data[20], data[19]) 
-#        print("leftX: %4i, leftY: %4i, rightX: %4i, rightY: %4i, cam: %4i" % (
-#           leftX, leftY, rightX, rightY, cam))
-        #(data[23]>>3)&31=data[23]
-        #print("{0:b}".format(data[23]))
-        #time.sleep(3)
-        buttons = [bool(int(x)) for x in bin((data[23] >> 3)&31)[2:].zfill(5)]
-        print(buttons)
+        rightX = word(data[12], data[11])
+        rightY = word(data[14], data[13]) 
+        leftY = word(data[16], data[15]) 
+        leftX = word(data[18], data[17]) 
+        cam = word(data[20], data[19]) 
+        print("leftX: %4i, leftY: %4i, rightX: %4i, rightY: %4i, cam: %4i" % (
+           leftX, leftY, rightX, rightY, cam))
+        rx = scale(leftX)
+        ry = scale(leftY)
+        ly = scale(rightX)
+        lx = scale(rightY)
+        cm = scale(cam)
+print("leftX: %4i, leftY: %4i, rightX: %4i, rightY: %4i, cam: %4i" % (
+           lX, lY, rX, rY, cm))
+#        (data[23]>>3)&31=data[23]
+#        print("{0:b}".format(data[23]))
+#        time.sleep(3)
+#        buttons = [bool(int(x)) for x in bin((data[23] >> 3)&31)[2:].zfill(5)]
+#        print(buttons)
--- Main Prosecsor/COMMUNICATION.py
+++ Main Prosecsor/COMMUNICATION.py
@@ -1,1 +1,19 @@
 #this is the code for communicating to the motor moduals
+
+import serial
+
+M1_Position = int()
+M1_Speed = int()
+M2_Position = int()
+M2_Speed = int()
+M3_Position = int()
+M3_Speed = int()
+M4_Position = int()
+M4_Speed = int()
+
+if __name__ == COMMUNICATION:
+
+
+
+    if M1_Position != 0 and M1_Speed != 0 and M2_Position != 0 and M2_Speed != 0 and M3_Position != 0 and M3_Speed != 0 and M4_Position != 0 and M4_Speed != 0:
+        

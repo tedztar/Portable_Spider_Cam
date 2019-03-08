8412cfe0618ca693cfe130f0b976746b03eba931
Timothy Stevenson <tedztar@gmail.com>
2019-03-08 19:23:39 +1300

    fixed an error with not allowing the camera to move into the X and Y max positions

--- Main Prosecsor/COMMUNICATION.py
+++ Main Prosecsor/COMMUNICATION.py
@@ -31,18 +31,18 @@ def comstart():
     global M4_Speed
     global mode
     while True:
-        if M1_Position != 0 and M1_Speed != 0 and M2_Position != 0 and M2_Speed != 0 and M3_Position != 0 and M3_Speed != 0 and M4_Position != 0 and M4_Speed != 0:
+        if M1_Position >= 0 and M1_Speed >= 0 and M2_Position >= 0 and M2_Speed >= 0 and M3_Position >= 0 and M3_Speed >= 0 and M4_Position >= 0 and M4_Speed >= 0:
             serial.write("1"+mode+ " {1:d} {2:d}").format(m1_position,m1_speed)
             serial.write("2r"+mode+ " {1:d} {2:d}").format(m2_position,m2_speed)
             serial.write("3r"+mode+ " {1:d} {2:d}").format(m3_position,m3_speed)
             serial.write("4r"+mode+ " {1:d} {2:d}").format(m4_position,m4_speed)
             serial.write("@")
 #            print(str(M1_Position)  + "" + str(M1_Speed)  + "" + str(M2_Position)  + "" + str(M2_Speed) + "" + str(M3_Position)  + "" + str(M3_Speed)  + "" + str(M4_Position)  + "" + str(M4_Speed))
-            M1_Position = 0
-            M1_Speed = 0
-            M2_Position = 0
-            M2_Speed = 0
-            M3_Position = 0
-            M3_Speed = 0
-            M4_Position = 0
-            M4_Speed = 0
+            M1_Position = -1
+            M1_Speed = -1
+            M2_Position = -1
+            M2_Speed = -1
+            M3_Position = -1
+            M3_Speed = -1
+            M4_Position = -1
+            M4_Speed = -1
